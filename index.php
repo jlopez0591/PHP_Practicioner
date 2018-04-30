93c470a8b727d9d5c1726a8c99ec040b7232dec0
Jose A. Lopez <jose.a.lopez0591@gmail.com>
2018-04-29 21:43:12 -0500

    Terminar Capitulo 6

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$names = [
+    "John Cena", 
+    "Undertaker", 
+    "Randy Orton",
+    "Edge",
+    "CM Punk"
+];
+
+foreach ($names as $name) {
+    echo $name;
+}
+
+require 'index.view.php';
