64c2ae87df42a50e9842db3bffb205a4466ec17a
Jose A. Lopez <jose.a.lopez0591@gmail.com>
2018-04-30 15:20:16 -0500

    Terminar Step 10: MySQL 101

--- index.php
+++ index.php
@@ -2,10 +2,5 @@
 
 require 'functions.php';
 
-$animals = ['dog', 'cat'];
-
-echo(check_age(21));
-dd($animals);
-
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -15,26 +15,6 @@
 </head>
 <body>
 <h1>Task for the Day</h1>
-<ul>
 
-    <li>
-        <strong>Name: </strong><?= $task['title'] ?>
-    </li>
-    <li>
-        <strong>Due Date: </strong><?= ucwords($task['due']) ?>
-    </li>
-    <li>
-        <strong>Person Responsible: </strong><?= $task['assigned_to'] ?>
-    </li>
-    <li>
-        <strong>Status: </strong>
-        <?php if ($task['completed']) : ?>
-            <span class="icon">'&#9989'</span>
-        <?php else: ?>
-            <span class="icon">Incomplete</span>
-        <?php endif; ?>
-    </li>
-
-</ul>
 </body>
 </html>
